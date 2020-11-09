dbbff54e9f76c7550ef3d3ecdbfe7b47ce2124af
Brenden <brenden111@hotmail.com>
2020-11-08 19:39:47 -0500

    updating init file

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 
 import os
 import time
+import itertools
 from datetime import datetime
 import pandas as pd
 import numpy as np
@@ -10,46 +11,101 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 '''
+Can we predict the programming language given the Title and the body
+
 Is there a corallation between title length, body length and creation date between rating?
 Can we predict the quality rating of a stack over flow question?
 
 creation_date_sanitized = np.array([int(datetime.strptime(date, '%Y-%m-%d %H:%M:%S').timestamp()) for date in data['CreationDate']])
 data['CreationDate'] = creation_date_sanitized
+'''
 
-'''
-'''
 #We want to be able to predict the tags for a post given the body and title
+
+#these are going to be the classifcaitions
+def tagCounts(data_obj):
+    dataTags = data_obj['Tags']
+    tagList = []
+    
+    for i in dataTags:
+        tagStr = str(i).replace(">",'').replace("<", ' ').split(" ")
+        for tagName in tagStr:
+            if tagName != '':
+                tagList.append(tagName)
+                
+    tag_counter = {}
+    for tag in tagList:
+        if tag in tag_counter:
+            tag_counter[tag] +=1
+        else:
+            tag_counter[tag] = 1
+            
+    values = []
+    for key, value in tag_counter.items():
+        values.append(value)
+        
+    most_common_tags = sorted(tag_counter, key=tag_counter.get, reverse=True)
+    top15 = most_common_tags[:15]
+    
+    super_list = []
+    tc_15 = values[:15]
+    tn_15 = most_common_tags[:15]
+
+    return sorted(tc_15, reverse=True), tn_15
+
+
 def getTop15MostCommonTags(data_obj):
     dataTags = data_obj['Tags']
-    for i in dataTags:
-        tagStr = str(i).split(">")
-        for j in tagStr:
-            j.strip("<")
-    print(tagStr)
+    tagList = []
     
-    try:
-       data_obj  = data_obj.drop(['Y'], axis = 1)
-       data_obj = data_obj.drop(['Creation_Date'], axis = 1)
-    except Exception as e:
-        print("One or more of the columns being dropped failed: {}".format(e))
-        pass
-    return dataTags
+    for i in dataTags:
+        tagStr = str(i).replace(">",'').replace("<", ' ').split(" ")
+        for tagName in tagStr:
+            if tagName != '':
+                tagList.append(tagName)
+                
+    tag_counter = {}
+    for tag in tagList:
+        if tag in tag_counter:
+            tag_counter[tag] +=1
+        else:
+            tag_counter[tag] = 1
+            
+    most_common_tags = sorted(tag_counter, key=tag_counter.get, reverse=True)
+    return most_common_tags[:15]
 
-'''
+
     
 #Preprocessing for post prediction
 trainData = pd.read_csv('D:\\INFR3700-FinalProject\\testing\\train.csv')
 validData = pd.read_csv('D:\\INFR3700-FinalProject\\testing\\valid.csv')
 
-
 frames = [trainData, validData]
 data = pd.concat(frames)
 
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(itertools.islice(iterable, n))
+
 mostCommonTags = getTop15MostCommonTags(data)
+tc = tagCounts(data)
+test_values = tc[0]
+test_keys = tc[1]
+
+res = {} 
+for key in test_keys: 
+    for value in test_values: 
+        res[key] = value 
+        test_values.remove(value) 
+        break
+    
+plt.figure(figsize=(15,6))
+plt.hist(list(res.keys()), weights=list(res.values()), rwidth=0.5)
+plt.title("Tags by populairty")
+plt.show()
 
 data = data.drop(['Id'], axis=1)
 data = data.drop(['Tags'], axis=1)
-
 #Test comment
 
 ls = []
@@ -71,6 +127,7 @@ data['Creation_Day'], data['Creation_Month'], data['Creation_Year'] = data['Crea
 data['Creation_Hour'], data['Creation_Minute'] =  data['CreationDate'].dt.hour,  data['CreationDate'].dt.minute
 data = data.drop(['CreationDate'], axis=1)
 
+
 from sklearn.preprocessing import LabelEncoder
 encoder = LabelEncoder()
 quality_cat = data["Y"]
@@ -81,45 +138,57 @@ print(encoder.classes_)
 # 1 LQ_CLOSE
 # 2 LQ_EDIT
 
-#Select all 2016 questions. we want to know of the 2016 year how many questions has a rating of HQ?
-years = [2019, 2020]
-for year in years:
-    select_date = data.loc[data['Creation_Year'] == year]
-    plt.hist(select_date['Y'], color='r', label="{} number of ratings ".format(str(year)))
-    plt.xlabel("Rating")
-    plt.ylabel("Number of ratings")
-    plt.xlim(0,2.1)
-    plt.legend()
+
+def exploringTheData_Part1():
+    #Select all 2016 questions. we want to know of the 2016 year how many questions has a rating of HQ?
+    years = [2020, 2016]
+    for year in years:
+        select_date = data.loc[data['Creation_Year'] == year]
+        plt.hist(select_date['Y'], color='r', label="{} number of ratings ".format(str(year)))
+        plt.xticks(ticks=[0,1,2], labels=["HQ","LQ_CLOSE","LQ_EDIT"])
+        plt.xlabel("Rating")
+        plt.title("Rating Count {}".format(str(year)))
+        plt.ylabel("Number of ratings")
+        plt.savefig("D:\\INFR3600-Final-Project\\hist_rating_count_{}.png".format(str(year)))
+        plt.xlim(0,2.1)
+        plt.legend()
+        #plt.savefig("D:\\INFR3600-Final-Project\\hist_rating_count_{}.png".format(year))
+        plt.show()
+        
+        
+    #Which programming languages/tags were most common in year x?
+    corr_matrix = data.corr()['Y']
+    print(corr_matrix.abs().sort_values(ascending=False))
+    max_cor_str = corr_matrix.abs().sort_values(ascending=False).index[1]
+    
+    plt.scatter(data['Title'], data['Body'], s=2, alpha=0.6)
+    plt.xlabel("Title Length")
+    plt.ylabel("Body Length")
+    plt.title("Body Length vs Title Length")
+    plt.savefig("D:\\INFR3600-Final-Project\\blen_vs_tlen.png")
     plt.show()
-
-corr_matrix = data.corr()['Y']
-print(corr_matrix.abs().sort_values(ascending=False))
-max_cor_str = corr_matrix.abs().sort_values(ascending=False).index[1]
-
-train, test = train_test_split(data, test_size=0.5, random_state=42)
-
-plt.scatter(data['Title'], data['Body'], s=2, alpha=0.6)
-plt.xlabel("")
-plt.show()
-
-#get contains question and has HQ rating
-cq_var = data[data['Contains_Question'] == 1].iloc[:,2]
-#get contains question and has LQ rating
-dcq_var= data[data['Contains_Question'] == 0].iloc[:,2]
-
-#of the titles that contain question marks, how many of those are high quality and how many our low quality
-plt.figure(figsize=(10,10))
-
-plt.subplot(2,2,1)
-plt.title("Contains ? and Ratings")
-plt.xticks(ticks=[0,1,2], labels=["HQ","LQ_CLOSE","LQ_EDIT"])
-plt.hist(cq_var)
-
-plt.subplot(2,2,2)
-plt.title("Does not contain ? and Ratings")
-plt.xticks(ticks=[0,1,2] ,labels=["HQ","LQ_CLOSE","LQ_EDIT"])
-plt.hist(dcq_var)
-plt.tight_layout()
-plt.show()
+    
+    #get contains question and has HQ rating
+    cq_var = data[data['Contains_Question'] == 1].iloc[:,2]
+    #get contains question and has LQ rating
+    dcq_var= data[data['Contains_Question'] == 0].iloc[:,2]
+    
+    #of the titles that contain question marks, how many of those are high quality and how many our low quality
+    plt.figure(figsize=(10,10))
+    
+    plt.subplot(2,2,1)
+    plt.title("Contains ? and Ratings")
+    plt.xticks(ticks=[0,1,2], labels=["HQ","LQ_CLOSE","LQ_EDIT"])
+    plt.hist(cq_var)
+    
+    plt.subplot(2,2,2)
+    plt.title("Does not contain ? and Ratings")
+    plt.xticks(ticks=[0,1,2] ,labels=["HQ","LQ_CLOSE","LQ_EDIT"])
+    plt.hist(dcq_var)
+    plt.tight_layout()
+    plt.show()
+    
+def predictingBodyLength(train_data):
+    pass
 
 
